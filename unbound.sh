ac6d85f152f5b7cb19f33985ef0255d08e03fa9a
Yann Castells <yann@castells.eu>
2019-11-03 18:05:07 +0100

    Change: Run unbound as non priviledged user
    Required changing the permissions of /etc/unbound
    and setting username in unbound.conf

--- unbound.sh
+++ unbound.sh
@@ -3,9 +3,8 @@
 # Download root hints
 wget -S -N https://www.internic.net/domain/named.cache -O /etc/unbound/root.hints
 
-# Adjust file settings
-#chmod g+w /etc/unbound/root.key
-#chown :unbound /etc/unbound/root.key
+# Adjust file settings; required for running as unbound
+chown -R unbound:unbound /etc/unbound
 
 # Initialize unbound
 unbound-checkconf
